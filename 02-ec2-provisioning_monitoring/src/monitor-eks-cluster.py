087354b102c2ca5b402567387e1f4786794a5780
hangrybear666 <corey.lauster@gmail.com>
2024-10-03 12:30:20 +0000

    Extended EKS monitoring statement

--- 02-ec2-provisioning_monitoring/src/monitor-eks-cluster.py
+++ 02-ec2-provisioning_monitoring/src/monitor-eks-cluster.py
@@ -1,17 +1,28 @@
 import boto3
+import pprint
+from datetime import datetime, timezone
 
 client = boto3.client('eks', region_name="eu-central-1")
 clusters = client.list_clusters()['clusters']
-
+KEY_ERROR_DEFAULT_FALLBACK = 'Not available.'
 for cluster in clusters:
     response = client.describe_cluster(
         name=cluster
     )
     cluster_info = response['cluster']
-    cluster_status = cluster_info['status']
-    cluster_endpoint = cluster_info['endpoint']
-    cluster_version = cluster_info['version']
-
-    print(f"Cluster {cluster} status is {cluster_status}")
-    print(f"Cluster endpoint: {cluster_endpoint}")
-    print(f"Cluster version: {cluster_version}")
+    time_difference = datetime.now(timezone.utc) - cluster_info.get('createdAt', datetime.now())
+    print(f"Cluster version      : {cluster_info.get('version', KEY_ERROR_DEFAULT_FALLBACK)}")
+    print(f"Cluster name         : {cluster_info.get('name', KEY_ERROR_DEFAULT_FALLBACK)}")
+    print(f"Cluster status       : {cluster_info.get('status', KEY_ERROR_DEFAULT_FALLBACK)}")
+    print(f"Time since creation  :"
+        f" {time_difference.days}days" \
+        f" {int(time_difference.seconds / 3600)}hrs" \
+        f" {int((time_difference.seconds % 3600) / 60)}min" \
+        f" {time_difference.seconds % 60}s"
+    )
+    print(f"Cluster endpoint     : {cluster_info.get('endpoint', KEY_ERROR_DEFAULT_FALLBACK)}")
+    print(f"---------------------------------------------------------------------------------------------------")
+    print(f"Cluster tags         : {pprint.pformat(cluster_info.get('tags', KEY_ERROR_DEFAULT_FALLBACK))}")
+    print(f"Amazon Resource Name : '{cluster_info.get('arn', KEY_ERROR_DEFAULT_FALLBACK)}'")
+    print(f"Subnet Ids           : {cluster_info.get('resourcesVpcConfig', {}).get('subnetIds', [])}")
+    print(f"Network Config       : {pprint.pformat(cluster_info.get('kubernetesNetworkConfig', {}))}")
